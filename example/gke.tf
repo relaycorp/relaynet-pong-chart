606c18eff3a60a0a068b10873cae44b373fd0feb
Gus Narea <gnarea@users.noreply.github.com>
2020-01-27 13:38:50 +0000

    feat: Create Terraform example (#5)

---
+++ example/gke.tf
@@ -0,0 +1,42 @@
+resource "google_container_cluster" "primary" {
+  name = "relaynet-pong"
+  location = "us-central1-a"
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count = 1
+
+  master_auth {
+    username = ""
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+
+  # Make cluster VPC-native (alias IP) so we can connect to GCP Memorystore for Redis
+  ip_allocation_policy {}
+}
+
+resource "google_container_node_pool" "primary" {
+  name = "relaynet-pong"
+  location = google_container_cluster.primary.location
+  cluster = google_container_cluster.primary.name
+  node_count = 2
+
+  node_config {
+    machine_type = "n1-standard-1"
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
---
+++ example/redis.tf
@@ -0,0 +1,12 @@
+output "redis_host" {
+  value = google_redis_instance.primary.host
+}
+
+resource "google_redis_instance" "primary" {
+  name = "pong-test"
+  memory_size_gb = 1
+
+  location_id = google_container_cluster.primary.location
+
+  redis_version = "REDIS_4_0"
+}
